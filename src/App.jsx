8c9f539f39ce3e4ed13d5083221b28a084500f5d
Chaitanya Babasaheb Zade <chaitanyazade042@gmail.com>
2026-01-04 16:10:27 +0530

    Switch from BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Footer from './components/Footer'
@@ -8,21 +9,20 @@ import Projects from './pages/Project'
 import About from './pages/About'
 import Contact from './pages/Contact'
 
-function App() {
+function App () {
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
+    <HashRouter>
+      <Navbar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer></Footer>
-      </BrowserRouter>
-    </>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </HashRouter>
   )
 }
 
