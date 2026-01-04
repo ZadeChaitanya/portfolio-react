f2c0218cc531c54c42a88b40d1e59b223f73d4f5
Chaitanya Babasaheb Zade <chaitanyazade042@gmail.com>
2026-01-04 17:01:27 +0530

    Fix Vite GitHub Pages deployment and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
 import React from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
-import Footer from './components/Footer'
 import Projects from './pages/Project'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Footer from './components/Footer'
 
-function App() {
+function App () {
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
+    <HashRouter>
+      <Navbar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer></Footer>
-      </BrowserRouter>
-    </>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </HashRouter>
   )
 }
 
