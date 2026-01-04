3aa59ac1985e97a704ed9a46ac94cae06ed18335
Chaitanya Babasaheb Zade <chaitanyazade042@gmail.com>
2026-01-04 17:05:37 +0530

    Fix Vite GitHub Pages deployment and routing

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/portfolio-react/'
-=======
-  base: '/portfolio-react/'   // 👈 VERY IMPORTANT
->>>>>>> f0df78611700f2c702af030632efd1915854aefe
 })
